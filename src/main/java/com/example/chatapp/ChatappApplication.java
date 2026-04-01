d20ef41208feb2c597db4ad1cdfaab6aaa59be5f
Trương Minh Sơn <163323332+TrgMSon@users.noreply.github.com>
2026-04-01 07:37:30 +0700

    Update ChatappApplication.java

--- src/main/java/com/example/chatapp/ChatappApplication.java
+++ src/main/java/com/example/chatapp/ChatappApplication.java
@@ -1,10 +1,20 @@
 package com.example.chatapp;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
-public class ChatappApplication { 
+public class ChatappApplication {
+	@PostConstruct
+	public void init() {
+		// Thiết lập múi giờ mặc định cho toàn bộ ứng dụng Java này
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatappApplication.class, args);
 	}
