ab4f40d1e1a17e22d585ef93cad800c3daaec955
TrgMSon <truongminhson1810@gmail.com>
2026-06-14 16:02:15 +0700

    fix signup

--- src/main/resources/static/scriptSignup.js
+++ src/main/resources/static/scriptSignup.js
@@ -7,38 +7,44 @@ const userForm = document.getElementById("userForm");
 const togglePasswordBtn = document.getElementById("toggle-password");
 
 userForm.addEventListener("submit", function (e) {
+    if (userName.value.trim() === "" || email.value.trim() === "" || password.value.trim() === "" || address.value.trim() === "") {
+        e.preventDefault();
+        alert("Vui lòng nhập đủ thông tin");
+        return;
+    }
+
     if (userName.value.trim().length > 50) {
+        e.preventDefault();
         alert("Tên người dùng tối đa 50 ký tự");
         userName.focus();
-        e.preventDefault();
         return;
     }
 
     if (email.value.trim().length > 45) {
+        e.preventDefault();
         alert("Email tối đa 45 ký tự");
         email.focus();
-        e.preventDefault();
         return;
     }
 
     if (password.value.trim().length > 45) {
+        e.preventDefault();
         alert("Mật khẩu tối đa 45 ký tự");
         password.focus();
-        e.preventDefault();
         return;
     }
 
     if (address.value.trim().length > 60) {
+        e.preventDefault();
         alert("Địa chỉ tối đa 60 ký tự");
         address.focus();
-        e.preventDefault();
         return;
     }
 
     if (bio.value.trim().length > 60) {
+        e.preventDefault();
         alert("Giới thiệu bản thân tối đa 60 ký tự");
         bio.focus();
-        e.preventDefault();
         return;
     }
 });
