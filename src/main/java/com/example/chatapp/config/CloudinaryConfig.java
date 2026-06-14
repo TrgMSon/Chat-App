70e30cf23ace69894af507ce98ffd953b744b93b
TrgMSon <truongminhson1810@gmail.com>
2026-06-14 16:11:09 +0700

    update cloudinary config

--- src/main/java/com/example/chatapp/config/CloudinaryConfig.java
+++ src/main/java/com/example/chatapp/config/CloudinaryConfig.java
@@ -16,8 +16,7 @@ public class CloudinaryConfig {
         // Load dotenv nhưng không bắt buộc phải có file
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-        // CÁCH LẤY BIẾN THÔNG MINH: 
-        // Thử lấy từ System Environment (Render/Railway) trước, nếu không có mới lấy từ .env (Local)
+        // Thử lấy từ System Environment (Render) trước, nếu không có mới lấy từ .env (Local)
         String cloudName = System.getenv("CLOUDINARY_NAME");
         if (cloudName == null) cloudName = dotenv.get("CLOUDINARY_NAME");
 
@@ -26,9 +25,6 @@ public class CloudinaryConfig {
 
         String apiSecret = System.getenv("CLOUDINARY_API_SECRET");
         if (apiSecret == null) apiSecret = dotenv.get("CLOUDINARY_API_SECRET");
-
-        // Debug (Chỉ nên dùng khi dev, xóa khi deploy thật để bảo mật)
-        System.out.println("Cloud Name: " + cloudName);
 
         if (cloudName == null || apiKey == null || apiSecret == null) {
             throw new RuntimeException("Cấu hình Cloudinary bị thiếu! Hãy kiểm tra Environment Variables hoặc file .env");
